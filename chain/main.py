e539a869a779ba17ff21029f13d0c8e4f70be2ae
unknown <github@chadam.pl>
2020-10-19 20:20:46 +0200

    update pubkey database and create test scenarios

--- chain/main.py
+++ chain/main.py
@@ -15,43 +15,35 @@ bob_public_key = bob_private_key.get_verifying_key()
 
 # Return public key of the current vote owner
 def get_owner(public_key):
-    if public_key == pkw_public_key:
-        return "PKW"
-    elif public_key == alice_public_key:
-        return "Alice"
-    elif public_key == bob_public_key:
-        return "Bob"
-    
+    database = {
+        utils.serialize(pkw_public_key): "PKW",
+        utils.serialize(alice_public_key): "Alice",
+        utils.serialize(bob_public_key): "Bob",
+    }
+    return database[utils.serialize(public_key)]
 
 class Transfer:
     def __init__(self, signature, public_key):
         self.signature = signature
         self.public_key = public_key
 
-
 class Vote:
     def __init__(self, transfers):
         self.transfers = transfers
+        self.validate()
 
     #Validate that the vote is valid
     def validate(self):
         # Get the first transfer and validate it - should be issued by the PKW
         first_transfer = self.transfers[0]
         message = utils.serialize(first_transfer.public_key)
-        try:
-            pkw_public_key.verify(first_transfer.signature, message)
-        except BadSignatureError:
-            return False
+        assert pkw_public_key.verify(first_transfer.signature, message)
         # Get the rest of transfers (if the voters gives away his vote to someone else)
         previous_transfer = first_transfer
         for next_transfer in self.transfers[1::]:
             message = transfer_message(previous_transfer.signature, next_transfer.public_key) 
-            try:
-                previous_transfer.public_key.verify(next_transfer.signature, message)
-            except BadSignatureError:
-                return False
+            assert previous_transfer.public_key.verify(next_transfer.signature, message)
             previous_transfer = next_transfer
-        return True
    
     # Return who is currently owning this vote
     def owner(self):
@@ -70,6 +62,7 @@ class Vote:
         print("PKW ", end="")
         for transfer in self.transfers:
             print("-> {} ".format( get_owner(transfer.public_key) ), end="")
+        print("")
 
 
 
@@ -88,30 +81,36 @@ def issue(public_key):
     vote = Vote([transfer])
     return vote
 
-# Give a vote to alice and verify it
-vote = issue(alice_public_key)
-print("This vote is owned by: {}".format(vote.owner()))
-print("\t\t\tis vote valid? {}".format(vote.validate()))
 
-# ==================================================================
-# Bob fakes his vote, should return error 
-bob_message = utils.serialize(bob_public_key)
-bob_signature = bob_private_key.sign(bob_message)
-bob_transfer = Transfer(bob_signature, bob_public_key)
-bob_vote = Vote([bob_transfer])
-#print("Is bob's fake vote valid? -> {}".format(bob_vote.validate()))
-# ==================================================================
+# Test scenario 1:
+# PKW gives alice the 'voting ticket', she forwards it to Bob and he sends it back to PKW
+# In the meantime, Bob tries to create his own 'voting ticket' out of thin air but fails to do so
+def test_scenario1():
+    # Give a vote to alice and verify it
+    vote = issue(alice_public_key)
+    print("This vote is owned by: {}".format(vote.owner()))
+    # Now let's pass the alice's vote to bob
+    vote.send(alice_private_key, bob_public_key)
+    print("This vote is owned by: {}".format(vote.owner()))
+    # Now the PKW gets it
+    vote.send(bob_private_key, pkw_public_key)
+    print("This vote is owned by: {}".format(vote.owner()))
+    # Check the history of the vote
+    vote.list_transfers()
+
+    # ==================================================================
+    # Bob fakes his vote, should return error 
+    bob_message = utils.serialize(bob_public_key)
+    bob_signature = bob_private_key.sign(bob_message)
+    bob_transfer = Transfer(bob_signature, bob_public_key)
+    try:
+        bob_vote = Vote([bob_transfer])
+    except:
+        print("=== Bob's vote is invalid! ===")
+    # ==================================================================
 
 
-# Now let's pass the alice's vote to bob
-vote.send(alice_private_key, bob_public_key)
-print("This vote is owned by: {}".format(vote.owner()))
-print("\t\t\tis vote valid? {}".format(vote.validate()))
+if __name__ == "__main__":
+    # Run Test scenario 1
+    test_scenario1()
 
-# Now the PKW gets it
-vote.send(bob_private_key, pkw_public_key)
-print("This vote is owned by: {}".format(vote.owner()))
-print("\t\t\tis vote valid? {}".format(vote.validate()))
-
-# Check the history of the vote
-vote.list_transfers()
