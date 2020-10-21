169590b10a78c01f1b1f4ceae359a1fa4c98bc64
unknown <github@chadam.pl>
2020-10-21 15:24:19 +0200

    Add writing and reading to/from disk

--- chain/utils.py
+++ chain/utils.py
@@ -5,3 +5,14 @@ def serialize(glos):
 
 def deserialize(serialized):
     return pickle.loads(serialized)
+
+def to_disk(data, filename):
+    serialized = serialize(data)
+    with open(filename, "wb") as f:
+        f.write(serialized)
+
+def from_disk(filename):
+    with open(filename, "rb") as f:
+        serialized = f.read()
+        return deserialize(serialized)
+
