40ffe2a95ca913ea725bccd06d6d2394f765b36e
unknown <github@chadam.pl>
2020-10-21 15:25:07 +0200

    Read keys from bootstrapping kit

--- chain/keys.py
+++ chain/keys.py
@@ -1,26 +1,35 @@
 from ecdsa import SigningKey, VerifyingKey, SECP256k1
 from ecdsa.keys import BadSignatureError
+from ecdsa.util import randrange_from_seed__trytryagain
 import utils
+import os
 
-#Create PKW keys
-pkw_private_key = SigningKey.generate(curve=SECP256k1)
-pkw_public_key = pkw_private_key.get_verifying_key()
+# Return shorter version of the key
+def short_key(public_key):
+    public_key_hex = public_key.to_string().hex()
+    return f"{public_key_hex[0:4]}...{public_key_hex[-5:]}"
 
-
-#Some voters
-alice_private_key = SigningKey.generate(curve=SECP256k1)
-alice_public_key = alice_private_key.get_verifying_key()
-
-bob_private_key = SigningKey.generate(curve=SECP256k1)
-bob_public_key = bob_private_key.get_verifying_key()
-
-# Return public key of the current vote owner
-# For debugging purposes only - HUGE privacy issue
-def get_owner(public_key):
-    database = {
-        utils.serialize(pkw_public_key): "PKW",
-        utils.serialize(alice_public_key): "Alice",
-        utils.serialize(bob_public_key): "Bob",
+# Prepare a sending commitment
+# i.e. tick the box on your voting ticket but dont put it in the ballot box yet
+def transfer_message(previous_signature, next_owner_public_key):
+    message = {
+            "previous_signature": previous_signature,
+            "next_owner_public_key": next_owner_public_key
     }
-    return database[utils.serialize(public_key)]
+    return utils.serialize(message)
+
+
+print("-" * 10)
+# Get the authorized committee public key list
+committee_public_keys = utils.from_disk("committee_pubkeys.votechain")
+for committee in committee_public_keys:
+    print("Committee {} with public key {}".format( committee, short_key(committee_public_keys[committee]) ))
+
+print("-" * 10)
+# Get the political parties public key list
+parties_public_keys = utils.from_disk("parties_pubkeys.votechain")
+for party in parties_public_keys:
+    print("Party {} with public key {}".format( party, short_key(parties_public_keys[party]) ))
+print("-" * 10)
+
 
