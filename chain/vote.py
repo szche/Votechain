8f421c50f0309734d18f1a1eb5f299ecb633a9bc
unknown <github@chadam.pl>
2020-10-21 15:24:36 +0200

    Move validate() to Committee

--- chain/vote.py
+++ chain/vote.py
@@ -7,32 +7,13 @@ class Vote:
     def __init__(self, transfers):
         self.transfers = transfers
         self.id = uuid4() # <- random and unique Vote id, used to check for duplicates
-        self.validate()
 
     def __eq__(self, other):
         return self.id == other.id and \
                 self.transfers == other.transfers
 
-    #Validate that the vote is valid
-    def validate(self):
-        # Get the first transfer and validate it - should be issued by the Committee 
-        first_transfer = self.transfers[0]
-        message = utils.serialize(first_transfer.public_key)
-        assert pkw_public_key.verify(first_transfer.signature, message)
-        # Get the rest of transfers (if voter delegates his vote)
-        previous_transfer = first_transfer
-        for next_transfer in self.transfers[1::]:
-            message = transfer_message(previous_transfer.signature, next_transfer.public_key) 
-            assert previous_transfer.public_key.verify(next_transfer.signature, message)
-            previous_transfer = next_transfer
-   
-    # Return who is currently owning this vote
-    def owner(self):
-        public_key = self.transfers[-1].public_key
-        return get_owner(public_key)
-
-    # Send this vote to someone else
-    def send(self, owner_private_key, recipient_public_key):
+    # Sign the transfer, but dont show it to the Committee yet!
+    def sign_transfer(self, owner_private_key, recipient_public_key):
         assert self.transfers[-1].public_key == owner_private_key.get_verifying_key()
         message = transfer_message(self.transfers[-1].signature, recipient_public_key)
         signature = owner_private_key.sign(message)
@@ -46,16 +27,5 @@ class Vote:
             print("-> {} ".format( get_owner(transfer.public_key) ), end="")
         print("")
 
-    def __str__(self):
-        return "Vote id: {}\nOwner: {}".format(self.id, self.owner())
 
 
-
-# Prepare a sending commitment
-# i.e. tick the box on your voting ticket but dont put it in the ballot box yet
-def transfer_message(previous_signature, next_owner_public_key):
-    message = {
-            "previous_signature": previous_signature,
-            "next_owner_public_key": next_owner_public_key
-    }
-    return utils.serialize(message)
