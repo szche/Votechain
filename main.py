459e1651f74bee57f14e694854ffb5b0f4b5986e
szche <szymon@chadam.pl>
2020-10-28 13:17:51 +0100

    Issue votes over the network

--- main.py
+++ main.py
@@ -49,28 +49,13 @@ class TCPHandler(socketserver.BaseRequestHandler):
 def case_voter():
     print("=" * 20)
     # Generate private key
-    #privkey_generator = input("Input your album number: ")
-    privkey_generator = 1234
+    privkey_generator = input("Input your album number: ")
+    #privkey_generator = 1234
     secexp = randrange_from_seed__trytryagain(privkey_generator, SECP256k1.order)
     private_key = SigningKey.from_secret_exponent(secexp, curve=SECP256k1)
     public_key = private_key.get_verifying_key()
     print("Your private key:\t {}".format(short_key(private_key)))
     print("Your public key:\t {}".format(short_key(public_key)))
-
-    #print("-" * 20)
-    #print("Committee {}".format(committee["name"]))
-    #print("Private key: {}".format( short_key(committee["privkey"]) ))
-    #print("Public key: {}".format( short_key(committee["pubkey"]) ))
-    #print("-" * 20)
-    #
-    #comm = Committee(committee["privkey"], committee["pubkey"])
-    #
-    #Issue a vote to my key and validate it
-    #my_vote = comm.issue(public_key)
-    #comm.validate_vote(my_vote)
-    #
-    #print("Owner: {}".format(short_key(comm.get_owner(my_vote))))
-    #print("I have {} votes".format(len(comm.fetch_vote(public_key))))
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(address)
@@ -80,7 +65,8 @@ def case_voter():
     serialized_message = utils.serialize(message)
     sock.sendall(serialized_message)
     message_data = sock.recv(100000)
-    print("Recieved: {}".format( utils.deserialize(message_data)  ))
+    data = utils.deserialize(message_data)
+    print("Recieved: {}".format(data))
 
 
 def case_committee():
@@ -94,6 +80,11 @@ def case_committee():
     print("-" * 20)
     
     comm = Committee(committee["privkey"], committee["pubkey"])
+    # Issue the votes to the voters
+    issue_votes = utils.from_disk("airdrop.votechain")
+    for voter in issue_votes:
+        vote = comm.issue(voter)
+        comm.validate_vote(vote)
     
     server = MyTCPServer(address, TCPHandler)
     server.serve_forever()
