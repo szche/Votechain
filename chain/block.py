4d77d7f8c5cf547cf1005162f2744f1b01c5c406
unknown <github@chadam.pl>
2020-10-30 16:16:10 +0100

    Chain the blocks

--- chain/block.py
+++ chain/block.py
@@ -4,12 +4,13 @@ import time
 from . import utils
 
 class Block:
-    def __init__(self, votes, timestamp=None, signature=None):
+    def __init__(self, votes, timestamp=None, signature=None, prev_sig=None):
         if timestamp == None:
             timestamp = time.time()
         self.timestamp = timestamp
         self.signature = signature
         self.votes = votes
+        self.prev_sig = prev_sig
 
     def __str__(self):
         output = ""
@@ -20,12 +21,16 @@ class Block:
             output += f'Signature: {self.signature}\n'
         else:
             output += f'Signature: {self.signature.hex()}\n'
+        if self.prev_sig == None:
+            output += f'Prev_signature: {self.prev_sig}\n'
+        else:
+            output += f'Prev_signature: {self.prev_sig.hex()}\n'
         output += '=======================\n'
         return output
     
     @property
     def message(self):
-        data = [self.votes, self.timestamp]
+        data = [self.votes, self.timestamp, self.prev_sig, ]
         return utils.serialize(data)
 
     def sign(self, private_key):
