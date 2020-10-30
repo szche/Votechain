9790bda62bb57079142995960ed25d1d981892f2
unknown <github@chadam.pl>
2020-10-30 17:11:50 +0100

    Clean up authorized key generation

--- voting_kit/generate_committees.py
+++ voting_kit/generate_committees.py
@@ -1,18 +1,41 @@
 import sys 
 import utils
 from ecdsa import SigningKey, VerifyingKey, SECP256k1
+from ecdsa.util import randrange_from_seed__trytryagain
+
+# Generates (privkey, pubkey) pair
+# Returns a tuple
+def create_keypair(generator):
+    secexp = randrange_from_seed__trytryagain(generator, SECP256k1.order)
+    private_key = SigningKey.from_secret_exponent(secexp, curve=SECP256k1)
+    public_key = private_key.get_verifying_key()
+    return (private_key, public_key)
+
 
 pubkeys = {}
 privkeys = {}
 for i in range(int(sys.argv[1])): 
     name = input("Whats the name of the committee no. {}: ".format(i+1))
-    com_privkey = SigningKey.generate(curve=SECP256k1)
-    com_pubkey = com_privkey.get_verifying_key()
-    pubkeys[name] = com_pubkey
-    privkeys[name] = com_privkey
+    password = input("What's the keypair generator? ")
+    keypair = create_keypair(password)
+    privkeys[name] = keypair[0]
+    pubkeys[name] = keypair[1]
 
-utils.to_disk(pubkeys, "committee_pubkeys.votechain")
-utils.to_disk(privkeys, "committee_privkeys.votechain")
+
+print("=" * 20)
+print(" === Private keys ===")
+for item in privkeys:
+    keys = privkeys[item].to_string().hex()
+    print(f'{item: <15} {keys}')
+
+print("=" * 20)
+print(" === Public keys ===")
+for item in pubkeys:
+    keys = pubkeys[item].to_string().hex()
+    print(f'{item: <15} {keys}')
+
+#Save only the public keys, use generators for private keys
+utils.to_disk(pubkeys, "committees.votechain")
 
 print("Successfully generated {} committees".format(sys.argv[1]))
 
--- voting_kit/generate_parties.py
+++ voting_kit/generate_parties.py
@@ -1,20 +1,42 @@
 import sys 
 import utils
 from ecdsa import SigningKey, VerifyingKey, SECP256k1
+from ecdsa.util import randrange_from_seed__trytryagain
+
+# Generates (privkey, pubkey) pair
+# Returns a tuple
+def create_keypair(generator):
+    secexp = randrange_from_seed__trytryagain(generator, SECP256k1.order)
+    private_key = SigningKey.from_secret_exponent(secexp, curve=SECP256k1)
+    public_key = private_key.get_verifying_key()
+    return (private_key, public_key)
 
 
-parties_pubkeys = {}
-parties_privkeys = {}
-
-for i in range(int(sys.argv[1])):
+pubkeys = {}
+privkeys = {}
+for i in range(int(sys.argv[1])): 
     name = input("Whats the name of the party no. {}: ".format(i+1))
-    party_privkey = SigningKey.generate(curve=SECP256k1)
-    party_pubkey = party_privkey.get_verifying_key()
-    parties_pubkeys[name] = party_pubkey
-    parties_privkeys[name] = party_privkey
+    password = input("What's the keypair generator? ")
+    keypair = create_keypair(password)
+    privkeys[name] = keypair[0]
+    pubkeys[name] = keypair[1]
 
-utils.to_disk(parties_pubkeys, "parties_pubkeys.votechain")
-utils.to_disk(parties_privkeys, "parties_privkeys.votechain")
+
+print("=" * 20)
+print(" === Private keys ===")
+for item in privkeys:
+    keys = privkeys[item].to_string().hex()
+    print(f'{item: <15} {keys}')
+
+print("=" * 20)
+print(" === Public keys ===")
+for item in pubkeys:
+    keys = pubkeys[item].to_string().hex()
+    print(f'{item: <15} {keys}')
+
+#Save only the public keys, use generators for private keys
+utils.to_disk(pubkeys, "parties.votechain")
+
 print("Successfully generated {} parties".format(sys.argv[1]))
 
     
